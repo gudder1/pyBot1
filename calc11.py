79dbbe538db2e263acec0d769435fdfee069385d
zerodott <ihor.lialiuk@gmail.com>
2021-11-17 19:03:18 +0200

    Create calc11.py

---
+++ calc11.py
@@ -0,0 +1,15 @@
+print('___Calc___')
+num1 = float(input('Введіть перше число:   '))
+num2 = float(input('Введіть друге число:   '))
+
+dod = num1 + num2
+vid = num1 - num2
+mn = num1 * num2
+dil = num1 / num2
+
+print("Сума двох чисел:  " + str(dod))
+print("Різниця двох чисел:   " + str(vid))
+print("Добуток двох чисел:   " + str(mn))
+print("Частка двох чисел:   " + str(dil))
+
+
